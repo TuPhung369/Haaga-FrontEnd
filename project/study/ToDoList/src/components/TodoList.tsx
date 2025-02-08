c563e7dc2462c904ca8ff05f8555280dfb5dfc33
TuPhung87 <tu.pt@ltv.dev>
2025-02-08 13:58:09 +0200

    completed todoList

--- project/study/ToDoList/src/components/TodoList.tsx
+++ project/study/ToDoList/src/components/TodoList.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
-import { Input, Button } from "antd";
-
+import { Input, Button, DatePicker } from "antd";
 import { Trash2 } from "lucide-react";
 
 interface Todo {
   id: number;
   text: string;
+  date: string;
 }
 
 export default function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [task, setTask] = useState("");
+  const [date, setDate] = useState<string>("");
 
   const addTodo = () => {
-    if (!task.trim()) return;
-    setTodos([...todos, { id: Date.now(), text: task }]);
+    if (!task.trim() || !date) return;
+    setTodos([...todos, { id: Date.now(), text: task, date }]);
     setTask("");
+    setDate("");
   };
 
   const removeTodo = (id: number) => {
@@ -28,10 +30,13 @@ export default function TodoList() {
       <div className="flex gap-2 mb-4">
         <Input
           value={task}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setTask(e.target.value)
-          }
+          onChange={(e) => setTask(e.target.value)}
           placeholder="Add a new task"
+        />
+        <DatePicker
+          onChange={(_, dateString) =>
+            setDate(Array.isArray(dateString) ? dateString[0] : dateString)
+          }
         />
         <Button onClick={addTodo}>Add</Button>
       </div>
@@ -39,13 +44,14 @@ export default function TodoList() {
         {todos.map((todo) => (
           <li
             key={todo.id}
-            className="flex justify-between items-center bg-gray-100 p-2 rounded-lg mb-2"
+            className="flex justify-between items-center bg-gray-100 p-2 rounded-lg mb-2 gap-4"
           >
-            <span className="mr-2">{todo.text}</span>
+            <span className="flex-grow text-blue-600">
+              {todo.text} - {todo.date}
+            </span>
             <Button
               variant="filled"
               size="small"
-              className="mr-5"
               onClick={() => removeTodo(todo.id)}
             >
               <Trash2 className="w-5 h-5 text-red-500" />
